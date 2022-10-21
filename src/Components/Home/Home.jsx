076617755d95afe8d1ad42aa348e8a74e4149808
jurgilijus <jurgissubacius@gmail.com>
2022-10-21 21:10:09 +0300

    home section done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,8 +6,19 @@ import './Home.css'
 
 function Home() {
   return (
-    <section className='home'>
-        
+    <section name="home" className='home'>
+        <div className='home-section'>
+          <p className='introduction'>Hi, my name is</p>
+          <h1>Jurgis Subačius</h1>
+          <h2>I'm a front-end developer.</h2>
+          <p>I'm front end developer specializing in building exceptional digital experiences. Currently, i'm focused on building responsive web applications.</p>
+          <div>
+            <button className='btn'>View work 
+            <span className='arrow-efect'><HiArrowNarrowRight className='ml-3 text-2xl'/>
+            </span>
+            </button>
+          </div>
+        </div>
     </section>
   )
 }
