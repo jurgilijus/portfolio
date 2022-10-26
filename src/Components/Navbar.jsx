fd48c16e294709967c3ea563fcaf63881296e9fb
jurgilijus <jurgissubacius@gmail.com>
2022-10-26 13:58:27 +0300

    social icons linked

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, {useState} from 'react'
-import {FaBars, FaTimes, FaGithub, FaLinkedin} from 'react-icons/fa'
+import {FaBars, FaTimes} from 'react-icons/fa'
 import {HiOutlineMail} from 'react-icons/hi'
-import {BsFillPersonLinesFill} from 'react-icons/bs'
 import Logo from '../Assets/64-64 white.net.png'
 import {Link} from 'react-scroll'
 import { navbarItems } from './NavbarItems'
-import Resume from '../Assets/Jurgis_Subačius_-_Junior_Software_Developer.pdf'
+import { navbarSocialItems } from './NavbarSocialItems'
 
 // CSS
 import "./Navbar.css"
@@ -50,15 +49,11 @@ function Navbar() {
         </ul>
         {/* Social icons */}
         <div className='social-icons'>
-            <ul className='social-icon'>
-                <li><a className='social-icon-text' href="https://www.linkedin.com/in/jurgis-suba%25C4%258Dius-5527056a/" target="_blank">Linkedin <FaLinkedin size={30}/></a></li>
+            {navbarSocialItems.map((navbarSocialItem) => 
+            <ul key={navbarSocialItem.id} className={navbarSocialItem.className}>
+                <li><a className='social-icon-text' href={navbarSocialItem.href} target="_blank" rel="noreferrer">{navbarSocialItem.title} {navbarSocialItem.icon}</a></li>
             </ul>
-            <ul className='social-icon git-color'>
-                <li><a className='social-icon-text' href="https://github.com/jurgilijus/portfolio.git" target="_blank">Github <FaGithub size={30}/></a></li>
-            </ul>
-            <ul className='social-icon res-color'>
-                <li><a className='social-icon-text' href={Resume} target="_blank">Resume <BsFillPersonLinesFill size={30}/></a></li>
-            </ul>
+            )}
             <ul className='social-icon mail-color'>
                 <li><Link className='social-icon-text' to="contact"  smooth={true} duration={500}>Email <HiOutlineMail size={30}/></Link></li>
             </ul>
