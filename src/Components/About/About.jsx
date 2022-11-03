76827ab2046949cf896e9c9858af5a7541f2f830
jurgilijus <jurgissubacius@gmail.com>
2022-11-03 12:47:10 +0200

    env

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -7,7 +7,12 @@ import './About.css'
 
 const rightVariant = {
   initial: { x: "0rem", transition: { duration: 0.5 } },
-  animate: { x: "-100%", display: "hidden"  }
+  animate: { x: "-100%"}
+}
+
+const leftVariant = {
+  initial: { x: "0rem", transition: { duration: 0.5 } },
+  animate: { x: "3rem"}
 }
 
 function About() {
@@ -23,18 +28,20 @@ function About() {
     <section name='about' className='about'>
         <div className='about-content'>
             <h4 className='text-4xl'>About</h4>
-            <motion.div ref={ref}
-                          variants={rightVariant}
-                          initial="hidden"
-                          animate={control} className='about-text'>
-                <p  
-                          className='about-text-intro'>
+            <div className='about-text'>
+                <motion.p ref={ref}
+                variants={rightVariant}
+                initial="hidden"
+                animate={control} className='about-text-intro'>
                   Hi. I'm Jurgis, nice to meet you. Please take a look around.
-                </p>
-                <p>
-                            I'am pasionate about building axcellent software that improves the live of those around me. I specialize in creating software for clients ranging from individuals and small-businesse all the way to large enterprise corporations. What would you do if you had a software expert available at your fingertips?
-                </p>
-            </motion.div>
+                </motion.p>
+                <motion.p ref={ref}
+                variants={leftVariant}
+                initial="hidden"
+                animate={control}>
+                  I'am pasionate about building axcellent software that improves the live of those around me. I specialize in creating software for clients ranging from individuals and small-businesse all the way to large enterprise corporations. What would you do if you had a software expert available at your fingertips?
+                </motion.p>
+            </div>
         </div>
     </section>
   )
