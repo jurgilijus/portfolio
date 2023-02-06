17a4f35b0a95257c00bcce879cbc1babde9de783
jurgilijus <jurgissubacius@gmail.com>
2023-02-06 16:19:28 +0200

    domain

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,50 +1,58 @@
-import React, {useEffect} from 'react'
-import {motion, useAnimation} from 'framer-motion'
-import {useInView} from 'react-intersection-observer'
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 
 // CSS
-import './About.css'
+import "./About.css";
 
 const rightVariant = {
   initial: { x: "0rem", transition: { duration: 0.5 } },
-  animate: { x: "-100%"}
-}
+  animate: { x: "-100%" },
+};
 
 const leftVariant = {
   initial: { x: "0rem", transition: { duration: 0.5 } },
-  animate: { x: "3rem"}
-}
+  animate: { x: "3rem" },
+};
 
 function About() {
-  
-  const control = useAnimation()
-  const [ref, inView] = useInView(false)
-  
+  const control = useAnimation();
+  const [ref, inView] = useInView(false);
+
   useEffect(() => {
-    inView ? control.start("initial") : control.start("animate")
+    inView ? control.start("initial") : control.start("animate");
   }, [control, inView]);
-  
+
   return (
-    <section name='about' className='about'>
-        <div>
-            <h4 className='text-4xl'>About</h4>
-            <div className='about-text'>
-                <motion.p ref={ref}
-                variants={rightVariant}
-                initial="hidden"
-                animate={control} className='about-text-intro'>
-                  Hi. I'm Jurgis, nice to meet you. Please take a look around.
-                </motion.p>
-                <motion.p ref={ref}
-                variants={leftVariant}
-                initial="hidden"
-                animate={control}>
-                  I'am pasionate about building axcellent software that improves the live of those around me. I specialize in creating software for clients ranging from individuals and small-businesse all the way to large enterprise corporations. What would you do if you had a software expert available at your fingertips?
-                </motion.p>
-            </div>
+    <section name="about" className="about">
+      <div>
+        <h4 className="text-4xl">About</h4>
+        <div className="about-text">
+          <motion.p
+            ref={ref}
+            variants={rightVariant}
+            initial="hidden"
+            animate={control}
+            className="about-text-intro"
+          >
+            Hi. I'm Jurgis, nice to meet you. Please take a look around.
+          </motion.p>
+          <motion.p
+            ref={ref}
+            variants={leftVariant}
+            initial="hidden"
+            animate={control}
+          >
+            I'am pasionate about building axcellent software that improves the
+            live of those around me. I specialize in creating software for
+            clients ranging from individuals and small-businesse all the way to
+            large enterprise corporations. What would you do if you had a
+            software expert available at your fingertips?
+          </motion.p>
         </div>
+      </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
