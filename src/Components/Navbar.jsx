6afb0115b50bb9beeb5dd34bbd224a8d0e0328f5
jurgilijus <jurgissubacius@gmail.com>
2023-02-09 17:02:00 +0200

    menu scroll hidden

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,6 +12,11 @@ import "./Navbar.css";
 function Navbar() {
   const [nav, setNav] = useState(false);
   const handleClick = () => setNav(!nav);
+  {
+    nav
+      ? (document.body.style.overflow = "hidden")
+      : (document.body.style.overflow = "auto");
+  }
 
   return (
     <header className="navbar">
